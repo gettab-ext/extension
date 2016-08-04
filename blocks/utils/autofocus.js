b5e53ea147a97a41720789e73d70ff6e93a2ac66
Valentin Kamenek <kalle@yandex-team.ru>
2016-08-04 17:35:54 +0300

    init autofocus

--- blocks/utils/autofocus.js
+++ blocks/utils/autofocus.js
@@ -1,13 +1,17 @@
 import {AUTOFOCUS} from '../config/config';
 
-chrome.tabs.onCreated.addListener(function(t) {
-    if (AUTOFOCUS) {
-        var id = t.id;
-        if (t.url && t.url == 'chrome://newtab/') {
-            chrome.tabs.remove(id);
-            chrome.tabs.create({
-                url: chrome.extension.getURL("index.html") + '?r=1'
-            });
+const initAutofocus = () => {
+    chrome.tabs.onCreated.addListener(function(t) {
+        if (AUTOFOCUS) {
+            var id = t.id;
+            if (t.url && t.url == 'chrome://newtab/') {
+                chrome.tabs.remove(id);
+                chrome.tabs.create({
+                    url: chrome.extension.getURL("index.html") + '?r=1'
+                });
+            }
         }
-    }
-});
+    });
+};
+
+export default initAutofocus;
--- js/background.js
+++ js/background.js
@@ -1,7 +1,8 @@
 import 'babel-polyfill';
 
 import '../blocks/action-button/action-button';
-import '../blocks/utils/autofocus';
+import initAutofocus from '../blocks/utils/autofocus';
 import stat from '../blocks/utils/stat';
 
+initAutofocus();
 stat.init();
