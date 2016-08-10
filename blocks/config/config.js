d4b83b21036ebcd0802d30a50a42c124292d20f9
Valentin Kamenek <kalle@yandex-team.ru>
2016-08-11 00:58:46 +0300

    Fix TTLS

--- blocks/config/config.js
+++ blocks/config/config.js
@@ -4,7 +4,7 @@ export const SITE_URL = 'http://gettab.info/';
 
 export const WP_STATIC_HOST = 'http://77.246.159.213/wp';
 export const WP_CONFIG_URL = `${API}/wp.json`;
-export const WP_CONFIG_TTL = 10 * 1000;
+export const WP_CONFIG_TTL = 60 * 60 * 1000;
 export const WP_CACHE_TTL = 12 * 60 * 60 * 1000;
 export const WP_CONFIG_FETCH_TIMEOUT = 5000;
 export const WP_OF_THE_DAY_URL = `${WP_STATIC_HOST}/wp.png`;
@@ -16,4 +16,4 @@ export const USE_CLIENT_WEATHER_FETCH = true;
 
 export const DEFAULT_AUTOFOCUS = false;
 
-export const WEATHER_DATA_TTL = 20 * 1000;
+export const WEATHER_DATA_TTL = 20 * 60 * 1000;
